d1ca37bec89f8c5f433819058243229a1ed0822c
Souta Kawahara <souta.kawahara@miraclelinux.com>
2024-11-26 17:16:48 +0900

    Add skelton Package class in models. And add test for models/build.py

---
+++ tests/models/test_build.py
@@ -0,0 +1,13 @@
+import unittest
+from alma_sbom.models import Build
+
+class TestPackage(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_constructor(self):
+        p = Build()
+
