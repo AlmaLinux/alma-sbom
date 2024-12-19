ec95da1b6155b41970a3bcc1b762bf1c7e1c7a27
Souta Kawahara <souta.kawahara@miraclelinux.com>
2024-12-19 14:32:25 +0900

    Init implementation of cyclonedex output
    Update formats layer:
     - update cyclonedx/models.py to add CDXFormatter and some write functions
    
    Note1:
    Old alma-sbom used get_instance(), but it is removed from cyclonedx-python-libs.
    So, this implementation don't use get_instance().
    More details: https://github.com/CycloneDX/cyclonedx-python-lib/pull/493
    
    Note2:
    Current writer in cyclonedx-python-libs(==2.7.1) doesn't have indent and pretty option.
    So, we need to implement writer using basic library.
    The newer cyclonedx-python-libs has that. These are likely rewirte after library update.

--- alma_sbom/formats/cyclonedx/models.py
+++ alma_sbom/formats/cyclonedx/models.py
@@ -1,26 +1,81 @@
 from enum import Enum
+from typing import Callable
+from logging import getLogger
+
 from cyclonedx.model.bom import Bom
+from cyclonedx.output import BaseOutput
+from cyclonedx.output.json import JsonV1Dot4
+from cyclonedx.output.xml import XmlV1Dot4
 
 from alma_sbom.data.models import Package, Build
 from alma_sbom.config.config import CommonConfig, SbomFileFormatType
 from ..document import Document as AlmasbomDocument
 
-class CDXDocument(AlmasbomDocument):
-    _bom: Bom
+_logger = getLogger(__name__)
 
-    def __init__(self):
-        self._bom = None
+
+class CDXFormatter:
+    FORMATTERS = {
+        SbomFileFormatType.JSON: JsonV1Dot4,
+        SbomFileFormatType.XML: XmlV1Dot4,
+    }
+    formatter: BaseOutput
+
+    ### TODO:
+    # Current writer in cyclonedx-python-libs(==2.7.1) doesn't have indent and pretty option.
+    # So, we need to implement writer using basic library.
+    # The newer cyclonedx-python-libs has that. These are likely rewirte after library update.
+    WRITERS = {
+        SbomFileFormatType.JSON: '_write_json',
+        SbomFileFormatType.XML: '_write_xml',
+    }
+    writer: Callable
+
+    def __init__(self, file_format: SbomFileFormatType) -> None:
+        self.formatter = self.FORMATTERS[file_format]
+        self.writer = getattr(self, self.WRITERS[file_format])
+
+    ###TODO:
+    # writer functions are likely renmove after library update.
+    def _write_json(self, bom: Bom) -> str:
+        import json
+        fff = self.formatter(bom)
+        output_str = fff.output_as_string()
+        json_output = json.loads(output_str)
+        return json.dumps(json_output, indent=4)
+
+    def _write_xml(self, bom: Bom) -> str:
+        import xml.dom.minidom
+        fff = self.formatter(bom)
+        output_str = fff.output_as_string()
+        xml_output = xml.dom.minidom.parseString(output_str)
+        return xml_output.toprettyxml()
+
+class CDXDocument(AlmasbomDocument):
+    bom: Bom
+    config: CommonConfig
+    formatter: CDXFormatter
+
+    def __init__(self, config: CommonConfig):
+        self.bom = Bom()
+        #self.bom.metadata.tools.components.add(cdx_lib_component())
+        self.config = config
+        self.formatter = CDXFormatter(self.config.sbom_type.file_format_type)
 
     @classmethod
     def from_package(cls, package: Package, config: CommonConfig) -> "CDXDocument":
-        doc = cls()
+        doc = cls(config)
         return doc
 
     @classmethod
     def from_build(cls, build: Build, Commonconfig) -> "CDXDocument":
-        doc = cls()
+        doc = cls(config)
         return doc
 
+    ###TODO:
+    # need to be rewriten after library update.
     def write(self) -> None:
-        raise NotImplementedError()
+        pretty_output = self.formatter.writer(self.bom)
+        with open(self.config.output_file, 'w') as fd:
+            fd.write(pretty_output)
 
