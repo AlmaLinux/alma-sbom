cf388ee5b2793b21c65f221d7fe2793709aeef26
Souta Kawahara <souta.kawahara@miraclelinux.com>
2025-01-02 23:56:23 +0900

    Fill publisher field in package component on cyclonedx
    Update constants.py to define ALMAOS_VENDOR
    Update formats layer:
     - update cyclonedx to fill publisher field in package component

--- alma_sbom/constants.py
+++ alma_sbom/constants.py
@@ -1,2 +1,3 @@
+ALMAOS_VENDOR = 'AlmaLinux OS Foundation'
 ALMAOS_SBOMLICENSE = 'CC0-1.0'
 ALMAOS_NAMESPACE = 'https://security.almalinux.org'
--- alma_sbom/formats/cyclonedx/component.py
+++ alma_sbom/formats/cyclonedx/component.py
@@ -3,30 +3,31 @@ from cyclonedx.model import HashAlgorithm, HashType
 from cyclonedx.model.component import Property as CDXProperty
 from cyclonedx.model.component import Component, ComponentType
 
+from alma_sbom import constants
 from alma_sbom.data.models.package import Hash, Algorithms
 from alma_sbom.data.models import Package, Build
 from alma_sbom.data.attributes.property import Property
 
 _logger = getLogger(__name__)
 
-def _make_hash(hash: Hash) -> HashType:
-    return HashType(
-        algorithm=HashAlgorithm(hash.algorithm.value),
-        hash_value=hash.value,
-    )
-
 def component_from_package(package: Package) -> Component:
     return Component(
         component_type=ComponentType('library'),
         name=package.package_nevra.name,
         version=package.package_nevra.get_EVR(),
-        #publisher=constants.ALMAOS_VENDOR,
+        publisher=constants.ALMAOS_VENDOR,
         hashes=[_make_hash(h) for h in package.hashs],
         cpe=package.package_nevra.get_cpe23(),
         purl=package.get_purl(),
         properties=[
             _make_property(prop) for prop in package.get_properties()
         ],
+    )
+
+def _make_hash(hash: Hash) -> HashType:
+    return HashType(
+        algorithm=HashAlgorithm(hash.algorithm.value),
+        hash_value=hash.value,
     )
 
 def _make_property(prop: Property) -> CDXProperty:
