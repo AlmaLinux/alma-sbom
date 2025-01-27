f57890d19ba16205fdd1c776f56a27524e5a8614
Souta Kawahara <souta.kawahara@miraclelinux.com>
2025-01-27 17:59:25 +0900

    Implement DocumentFactory in cli/factory/documents/factory.py

--- alma_sbom/cli/commands/build.py
+++ alma_sbom/cli/commands/build.py
@@ -3,31 +3,30 @@ from logging import getLogger
 from typing import Callable
 
 from alma_sbom.data import Build, ImmudbCollector, AlbsCollector
-from alma_sbom.formats import Document, document_factory
 from .commands import SubCommand
 from ..config.config import CommonConfig
 from ..config.models.build import BuildConfig
 
-from alma_sbom.cli.factory.collectors.factory import CollectorFactory
+from alma_sbom.cli.factory import CollectorFactory, DocumentFactory
 
 _logger = getLogger(__name__)
 
 class BuildCommand(SubCommand):
     config: BuildConfig
     collector_factory: CollectorFactory
-    doc: Document
+    document_factory: DocumentFactory
     runner: Callable
 
     def __init__(self, base: CommonConfig, args: argparse.Namespace) -> None:
         self.config = BuildConfig.from_base_args(base, args)
         self.collector_factory = CollectorFactory(self.config)
+        self.document_factory = DocumentFactory(self.config)
         self._select_runner()
 
     def run(self) -> int:
         build = self.runner()
-        document_class = document_factory(self.config.sbom_type.record_type)
-        self.doc = document_class.from_build(build, self.config.sbom_type.file_format_type)
-        self.doc.write(self.config.output_file)
+        doc = self.document_factory.gen_from_build(build)
+        doc.write(self.config.output_file)
         return 0
 
     def _select_runner(self) -> None:
--- alma_sbom/cli/commands/package.py
+++ alma_sbom/cli/commands/package.py
@@ -3,33 +3,30 @@ from logging import getLogger
 from typing import Callable
 
 from alma_sbom.data import Package, ImmudbCollector
-from alma_sbom.formats import Document, document_factory
 from .commands import SubCommand
 from ..config.config import CommonConfig
 from ..config.models.package import PackageConfig
 
-from alma_sbom.cli.factory.collectors.factory import CollectorFactory
+from alma_sbom.cli.factory import CollectorFactory, DocumentFactory
 
 _logger = getLogger(__name__)
 
 class PackageCommand(SubCommand):
     config: PackageConfig
     collector_factory: CollectorFactory
-    doc: Document
+    document_factory: DocumentFactory
     runner: Callable
 
     def __init__(self, base: CommonConfig, args: argparse.Namespace) -> None:
         self.config = PackageConfig.from_base_args(base, args)
         self.collector_factory = CollectorFactory(self.config)
+        self.document_factory = DocumentFactory(self.config)
         self._select_runner()
 
     def run(self) -> int:
         package = self.runner()
-
-        document_class = document_factory(self.config.sbom_type.record_type)
-        self.doc = document_class.from_package(package, self.config.sbom_type.file_format_type)
-        self.doc.write(self.config.output_file)
-
+        doc = self.document_factory.gen_from_package(package)
+        doc.write(self.config.output_file)
         return 0
 
     def _select_runner(self) -> None:
