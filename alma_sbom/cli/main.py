fdae9c66f22a194f520c024d86a86ff5f37a0428
Souta Kawahara <souta.kawahara@miraclelinux.com>
2025-01-30 11:47:40 +0900

    Move logging arguments settings from cli/main.py to cli/logging.py

--- alma_sbom/cli/main.py
+++ alma_sbom/cli/main.py
@@ -1,10 +1,10 @@
 import argparse
 import sys
-from logging import DEBUG, INFO, WARNING, getLogger, Logger
+from logging import getLogger
 
 from alma_sbom.type import SbomType
 
-from .logging import Logging
+from .logging import Logging, add_logging_arguments
 from .commands import SubCommand, command_factory
 from .config import CommonConfig, add_config_arguments
 
@@ -16,7 +16,7 @@ class Main:
     config: CommonConfig
 
     def __init__(self, args: list[str]) -> None:
-        parser = create_parser()
+        parser = self.create_parser()
         self.args = parser.parse_args(args)
         logging = Logging(loglevel=self.args.loglevel)
         self.config = CommonConfig.from_args(self.args)
@@ -25,30 +25,12 @@ class Main:
     def run(self) -> int:
         return self.command.run()
 
-def create_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(description='alma-sbom')
-    add_config_arguments(parser)
-
-    ### logging settings ###
-    parser.add_argument(
-        '--verbose',
-        help=(
-            'Print verbose output'
-        ),
-        required=False,
-        default=WARNING,
-        action='store_const', dest='loglevel', const=INFO,
-    )
-    parser.add_argument(
-        '--debug',
-        help=(
-            'Print debug log'
-        ),
-        required=False,
-        action='store_const', dest='loglevel', const=DEBUG,
-    )
-
-    return parser
+    @staticmethod
+    def create_parser() -> argparse.ArgumentParser:
+        parser = argparse.ArgumentParser(description='alma-sbom')
+        add_config_arguments(parser)
+        add_logging_arguments(parser)
+        return parser
 
 def cli_main():
     args = sys.argv[1:]
