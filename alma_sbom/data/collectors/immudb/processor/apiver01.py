c8c7c4ff233fdadbabf4ed52bb76b34bc49b0da6
Souta Kawahara <souta.kawahara@miraclelinux.com>
2025-02-14 00:32:13 +0900

    Fix to use get method to get value from dict instance(=immudb_metadata) in data/collectors/immudb/processor/apiver{01, 02}.py

--- alma_sbom/data/collectors/immudb/processor/apiver01.py
+++ alma_sbom/data/collectors/immudb/processor/apiver01.py
@@ -20,7 +20,7 @@ class DataProcessor01(DataProcessor):
         return '0.1'
 
     def get_package(self) -> Package:
-        package_name = self.immudb_info['Name']
+        package_name = self.immudb_info.get('Name')
         package_nevra = PackageNevra.from_str_nothas_epoch(package_name)
         pkg_props, build_props, sbom_props = self._properties_from_immudb_info_about_package(package_nevra)
 
@@ -28,7 +28,7 @@ class DataProcessor01(DataProcessor):
             package_nevra = package_nevra,
             source_rpm = None,
             hashs = [self.hash],
-            package_timestamp = self.immudb_info['timestamp'],
+            package_timestamp = self.immudb_info.get('timestamp'),
             package_properties = pkg_props,
             build_properties = build_props,
             sbom_properties = sbom_props,
@@ -44,37 +44,38 @@ class DataProcessor01(DataProcessor):
             version=package_nevra.version,
             release=package_nevra.release,
             arch=package_nevra.arch,
-            buildhost=self.immudb_metadata['build_host'],
+            buildhost=self.immudb_metadata.get('build_host'),
             sourcerpm=None,
-            timestamp=self.immudb_info['timestamp'],
+            timestamp=self.immudb_info.get('timestamp'),
         )
 
         build_src_props = None
-        if self.immudb_metadata['source_type'] == 'git':
+        source_type = self.immudb_metadata.get('source_type')
+        if source_type == 'git':
             build_src_props = GitSourceProperties(
-               git_url=self.immudb_metadata['git_url'],
-               git_commit=self.immudb_metadata['git_url'],
-               git_ref=self.immudb_metadata['git_ref'],
-               git_commit_immudb_hash=self.immudb_metadata['alma_commit_sbom_hash'],
+               git_url=self.immudb_metadata.get('git_url'),
+               git_commit=self.immudb_metadata.get('git_url'),
+               git_ref=self.immudb_metadata.get('git_ref'),
+               git_commit_immudb_hash=self.immudb_metadata.get('alma_commit_sbom_hash'),
             )
-        elif self.immudb_metadata['source_type'] == 'srpm':
+        elif source_type == 'srpm':
             build_src_props = SrpmSourceProperties(
-               srpm_url=self.immudb_metadata['srpm_url'],
-               srpm_checksum=self.immudb_metadata['srpm_sha256'],
-               srpm_nevra=self.immudb_metadata['srpm_nevra'],
+               srpm_url=self.immudb_metadata.get('srpm_url'),
+               srpm_checksum=self.immudb_metadata.get('srpm_sha256'),
+               srpm_nevra=self.immudb_metadata.get('srpm_nevra'),
             )
         else:
-            raise ValueError(f"Unknown source_type: {self.immudb_metadata['source_type']}")
+            raise ValueError(f'Unknown source_type: {source_type}')
         build_props = BuildProperties(
-            target_arch=self.immudb_metadata['build_arch'],
+            target_arch=self.immudb_metadata.get('build_arch'),
             package_type='rpm',
-            build_id=self.immudb_metadata['build_id'],
+            build_id=self.immudb_metadata.get('build_id'),
             ### TODO:
             # set collect build_url
             # how to get albs_url??
             #build_url=f'{albs_url}/build/{immudb_metadata["build_id"]}',
             build_url='https://dummy.almalinux.org',  ### dummy
-            author=self.immudb_metadata['built_by'],
+            author=self.immudb_metadata.get('built_by'),
             source=build_src_props,
         )
 
--- alma_sbom/data/collectors/immudb/processor/apiver02.py
+++ alma_sbom/data/collectors/immudb/processor/apiver02.py
@@ -23,21 +23,22 @@ class DataProcessor02(DataProcessor):
     def get_package(self) -> Package:
         package_nevra = PackageNevra(
             ### NOTE:
-            # In alma-sbom, null epoch is represented as 0
-            # Please see normalize_epoch implementation for more details
-            epoch = normalize_epoch(self.immudb_metadata['epoch']),
-            name = self.immudb_metadata['name'],
-            version = self.immudb_metadata['version'],
-            release = self.immudb_metadata['release'],
-            arch = self.immudb_metadata['arch'],
+            # Due to uncertainty about whether missing epoch field in the database'
+            # can be treated as equivalent to 0, Now we store None for missing epoch
+            # field at this time.
+            epoch = normalize_epoch(self.immudb_metadata['epoch']) if 'epoch' in self.immudb_metadata else None,
+            name = self.immudb_metadata.get('name'),
+            version = self.immudb_metadata.get('version'),
+            release = self.immudb_metadata.get('release'),
+            arch = self.immudb_metadata.get('arch'),
         )
         pkg_props, build_props, sbom_props = self._properties_from_immudb_info_about_package()
 
         return Package(
             package_nevra = package_nevra,
-            source_rpm = self.immudb_metadata['sourcerpm'],
+            source_rpm = self.immudb_metadata.get('sourcerpm'),
             hashs = [self.hash],
-            package_timestamp = self.immudb_info['timestamp'],
+            package_timestamp = self.immudb_info.get('timestamp'),
             package_properties = pkg_props,
             build_properties = build_props,
             sbom_properties = sbom_props,
@@ -50,47 +51,49 @@ class DataProcessor02(DataProcessor):
         ]:
         pkg_props = PackageProperties(
             ### NOTE:
-            # In alma-sbom, null epoch is represented as 0
-            # Please see normalize_epoch implementation for more details
-            epoch=normalize_epoch(self.immudb_metadata['epoch']),
-            version=self.immudb_metadata['version'],
-            release=self.immudb_metadata['release'],
-            arch=self.immudb_metadata['arch'],
-            buildhost=self.immudb_metadata['build_host'],
-            sourcerpm=self.immudb_metadata['sourcerpm'],
-            timestamp=self.immudb_info['timestamp'],
+            # Due to uncertainty about whether missing epoch field in the database'
+            # can be treated as equivalent to 0, Now we store None for missing epoch
+            # field at this time.
+            epoch = normalize_epoch(self.immudb_metadata['epoch']) if 'epoch' in self.immudb_metadata else None,
+            version=self.immudb_metadata.get('version'),
+            release=self.immudb_metadata.get('release'),
+            arch=self.immudb_metadata.get('arch'),
+            buildhost=self.immudb_metadata.get('build_host'),
+            sourcerpm=self.immudb_metadata.get('sourcerpm'),
+            timestamp=self.immudb_info.get('timestamp'),
         )
 
         build_src_props = None
-        if self.immudb_metadata['source_type'] == 'git':
+        source_type = self.immudb_metadata.get('source_type')
+        if source_type == 'git':
             build_src_props = GitSourceProperties(
-               git_url=self.immudb_metadata['git_url'],
-               git_commit=self.immudb_metadata['git_url'],
-               git_ref=self.immudb_metadata['git_ref'],
-               git_commit_immudb_hash=self.immudb_metadata['alma_commit_sbom_hash'],
+               git_url=self.immudb_metadata.get('git_url'),
+               git_commit=self.immudb_metadata.get('git_url'),
+               git_ref=self.immudb_metadata.get('git_ref'),
+               git_commit_immudb_hash=self.immudb_metadata.get('alma_commit_sbom_hash'),
             )
-        elif immudb_metadata['source_type'] == 'srpm':
+        elif source_type == 'srpm':
             build_src_props = SrpmSourceProperties(
-               srpm_url=self.immudb_metadata['srpm_url'],
-               srpm_checksum=self.immudb_metadata['srpm_sha256'],
-               srpm_nevra=self.immudb_metadata['srpm_nevra'],
+               srpm_url=self.immudb_metadata.get('srpm_url'),
+               srpm_checksum=self.immudb_metadata.get('srpm_sha256'),
+               srpm_nevra=self.immudb_metadata.get('srpm_nevra'),
             )
         else:
-            raise ValueError(f"Unknown source_type: {self.immudb_metadata['source_type']}")
+            raise ValueError(f'Unknown source_type: {source_type}')
         build_props = BuildProperties(
-            target_arch=self.immudb_metadata['build_arch'],
+            target_arch=self.immudb_metadata.get('build_arch'),
             package_type='rpm',
-            build_id=self.immudb_metadata['build_id'],
+            build_id=self.immudb_metadata.get('build_id'),
             ### TODO:
             # set collect build_url
             # How to get albs_url??
             #build_url=f'{albs_url}/build/{immudb_metadata["build_id"]}',
             build_url='https://dummy.almalinux.org',  ### dummy
-            author=self.immudb_metadata['built_by'],
+            author=self.immudb_metadata.get('built_by'),
             source=build_src_props,
         )
 
-        sbom_props = SBOMProperties(immudb_hash=self.immudb_info['Hash'])
+        sbom_props = SBOMProperties(immudb_hash=self.immudb_info.get('Hash'))
 
         return pkg_props, build_props, sbom_props
 
