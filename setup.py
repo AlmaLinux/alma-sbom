f504ba73ef36f361d4af3df4af219762cb92bbce
Souta Kawahara <souta.kawahara@miraclelinux.com>
2025-02-15 19:16:27 +0900

    Update install_requires in setup.py & Requirements in README.md to add rpm module
    Please see fllowing for more details: https://github.com/AlmaLinux/alma-sbom/pull/40#discussion_r1865952570

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
+import sys
 from setuptools import setup, find_namespace_packages
 
 from alma_sbom._version import __version__
+
+def get_install_requires():
+    requires=[
+        'requests>=2.20.0',
+        'cyclonedx-python-lib==2.7.1',
+        'spdx-tools==0.8',
+        'urllib3<2.0',
+        'packageurl-python==0.10.3',
+        'GitPython==3.1.29',
+        'immudb_wrapper @ git+https://github.com/AlmaLinux/immudb-wrapper.git@0.1.5#egg=immudb_wrapper',
+    ]
+
+    is_venv = sys.prefix != sys.base_prefix
+    if is_venv:
+        requires.append('rpm==0.3.1')
+    else: # not is_venv
+        requires.append('rpm>=4.14')
+
+    return requires
 
 setup(
     name="alma-sbom",
@@ -19,15 +39,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=find_namespace_packages(include=['alma_sbom*']),
-    install_requires=[
-        'requests>=2.20.0',
-        'cyclonedx-python-lib==2.7.1',
-        'spdx-tools==0.8',
-        'urllib3<2.0',
-        'packageurl-python==0.10.3',
-        'GitPython==3.1.29',
-        'immudb_wrapper @ git+https://github.com/AlmaLinux/immudb-wrapper.git@0.1.5#egg=immudb_wrapper',
-    ],
+    install_requires=get_install_requires(),
     python_requires=">=3.9",
     entry_points={
         'console_scripts': [
